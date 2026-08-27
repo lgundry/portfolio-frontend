7ece26acaad899efdb3dbce9d4bbe31591395ab3
Logan Gundry <74257349+lgundry@users.noreply.github.com>
2026-08-27 08:41:23 -0700

    Make ParallaxBackground reusable: accept dark/light image URLs via props and update App to pass dark/light backgrounds (fixes light-mode background swap)

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,14 @@ import ParallaxBackground from 'components/parallax/ParallaxBackground.component
 import Projects from 'components/projects/Projects.component'
 import Footer from 'components/footer/Footer.component'
 
+// Import the two background variants and pass them into the reusable parallax component
+import bgDark from 'assets/images/pexels-enginakyurt-5200063.jpg'
+import bgLight from 'assets/images/pexels-enginakyurt-5200063-inverted.jpg'
+
 function App() {
   return (
     <>
-      <ParallaxBackground speed={1.25} />
+      <ParallaxBackground speed={1.25} darkImage={bgDark} lightImage={bgLight} />
       <Navigation />
       <Intro />
       <About />
